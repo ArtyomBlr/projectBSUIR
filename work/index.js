2ba16a75e76126d81e24ae9d4f0bf8391d02da44
ArtyomBlr <59910600+ArtyomBlr@users.noreply.github.com>
2020-03-17 13:03:04 +0300

    feat: add triangles

--- work/index.js
+++ work/index.js
@@ -4,7 +4,104 @@ stage = new createjs.Stage(canvas);
 TweenMax.ticker.addEventListener("tick", stage.update, stage);
 TweenMax.ticker.fps(60);
 
-var rect = new createjs.Shape();
+// background 1
+
+var background1 = new createjs.Shape();
+background1.graphics
+.beginFill("#043245")
+.rect(0, 0, 800, 600)
+.endFill();
+stage.addChild(background1);
+
+// triangle1
+
+var triangle1 = new createjs.Shape();
+stage.addChild(triangle1);
+triangle1.x = -600;
+triangle1.y = -1600;
+
+var ta = {
+	p0x: 0,
+	p0y: 0,
+	p1x: 2000,
+	p1y: 0,
+	p2x: 1000,
+	p2y: 1600,
+};
+
+triangle1.graphics
+.beginFill("#FCFCFC")
+.mt(ta.p0x, ta.p0y)
+.lt(ta.p1x, ta.p1y)
+.lt(ta.p2x, ta.p2y)
+.cp()
+.endFill();
+
+// triangle2
+
+var triangle2 = new createjs.Shape();
+stage.addChild(triangle2);
+triangle2.x = -600;
+triangle2.y = -1800;
+
+
+triangle2.graphics
+.beginFill("#E83530")
+.mt(ta.p0x, ta.p0y)
+.lt(ta.p1x, ta.p1y)
+.lt(ta.p2x, ta.p2y)
+.cp()
+.endFill();
+
+// triangle3
+
+var triangle3 = new createjs.Shape();
+stage.addChild(triangle3);
+triangle3.x = -600;
+triangle3.y = -2000;
+
+triangle3.graphics
+.beginFill("#F7A91B")
+.mt(ta.p0x, ta.p0y)
+.lt(ta.p1x, ta.p1y)
+.lt(ta.p2x, ta.p2y)
+.cp()
+.endFill();
+
+// triangle4
+
+var triangle4 = new createjs.Shape();
+stage.addChild(triangle4);
+triangle4.x = -600;
+triangle4.y = -2200;
+
+triangle4.graphics
+.beginFill("#F7D9A7")
+.mt(ta.p0x, ta.p0y)
+.lt(ta.p1x, ta.p1y)
+.lt(ta.p2x, ta.p2y)
+.cp()
+.endFill();
+
+TweenMax.to(triangle1, 2, {y: 0, ease: Power1.easeInOut});
+TweenMax.to(triangle2, 2.1, {y: 0, ease: Power1.easeInOut});
+TweenMax.to(triangle3, 2.2, {y: 0, ease: Power1.easeInOut});
+TweenMax.to(triangle4, 2.3, {y: 0, ease: Power1.easeInOut});
+
+
+
+
+/*var tl = new TimelineMax({repeat : 0});
+
+tl.to(triangle1, 1, {y: 250, ease: Power1.easeInOut});
+tl.to(triangle2, 1, {y: 250, ease: Power1.easeInOut});
+tl.to(triangle3, 1, {y: 250, ease: Power1.easeInOut});
+
+
+
+// rectangle 
+
+/*var rect = new createjs.Shape();
 stage.addChild(rect);
 rect.x = 320;
 rect.y = 220;
@@ -29,9 +126,9 @@ rect.graphics
 .lt(a.p2x, a.p2y)
 .lt(a.p3x, a.p3y)
 .cp()
-.endFill();
+.endFill();*/
 
-TweenMax.to(rect, 2, {scaleX:1, scaleY:1, rotation:315, ease: Power1.easeInOut});
+//TweenMax.to(rect, 2, {scaleX:1, scaleY:1, rotation:315, ease: Power1.easeInOut});
 
 
 
